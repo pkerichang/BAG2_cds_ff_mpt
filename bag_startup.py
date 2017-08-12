76ab09dab8797d28d2055f5e49e181bdba063029
Eric Chang <pkerichang@berkeley.edu>
2017-08-12 00:02:32 -0700

    1. remove unncessary files from repo. 2. added bag_testbenches_ec to bag_startup.py 3. added transistor characterization specs. 4. added hvt transistors to BAG_prim. 5. update BAG_prim netlist format.

--- bag_startup.py
+++ bag_startup.py
@@ -3,4 +3,6 @@ import sys
 
 sys.path.append(os.environ['BAG_FRAMEWORK'])
 sys.path.append(os.path.join(os.environ['BAG_WORK_DIR'], 'BAG2_TEMPLATES_EC'))
+sys.path.append(os.path.join(os.environ['BAG_WORK_DIR'], 'bag_testbenches_ec'))
 sys.path.append(os.environ['BAG_TECH_CONFIG_DIR'])
+
