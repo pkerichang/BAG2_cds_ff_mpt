b6aa9797a466f90e0d7045eff69ba7014d1ee96d
Eric Chang <pkerichang@berkeley.edu>
2017-08-14 21:18:56 -0700

    1. added BAG_XBase_demo to bag_startup.py 2. adding demo specifications file.

--- bag_startup.py
+++ bag_startup.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 sys.path.append(os.environ['BAG_FRAMEWORK'])
-sys.path.append(os.path.join(os.environ['BAG_FRAMEWORK'], 'tutorial'))
 sys.path.append(os.path.join(os.environ['BAG_WORK_DIR'], 'BAG2_TEMPLATES_EC'))
 sys.path.append(os.path.join(os.environ['BAG_WORK_DIR'], 'bag_testbenches_ec'))
+sys.path.append(os.path.join(os.environ['BAG_WORK_DIR'], 'BAG_XBase_demo'))
 sys.path.append(os.environ['BAG_TECH_CONFIG_DIR'])
 
