61121925af3c543f1caf8b96f63dd2d29572f6f0
Eric Chang <pkerichang@gmail.com>
2017-11-28 08:27:15 -0800

    updated transistor characterization scripts.

--- scripts_demo/mos_query.py
+++ scripts_demo/mos_query.py
@@ -4,118 +4,100 @@ import pprint
 
 import numpy as np
 import matplotlib.pyplot as plt
+# noinspection PyUnresolvedReferences
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from matplotlib import ticker
 
-import bag
-import bag.tech.mos
+from verification_ec.mos.query import MOSDBDiscrete
+
+interp_method = 'spline'
+spec_file = 'specs_char/nch_w4.yaml'
+env_default = 'tt'
+intent = 'standard'
 
 
-def plot_data(name='gm', mos_type='nch', vgs_min=None, vds_min=None):
+def query(vgs=None, vds=None, vbs=0.0, vstar=None, env_list=None):
     """Get interpolation function and plot/query."""
-    env_list = ['tt', 'ff', 'ss', 'fs', 'sf', 'ff_hot', 'ss_hot']
-    l = 60e-9
-    intent = 'lvt'
-    w = 0.5e-6
+
+    spec_list = [spec_file]
+    if env_list is None:
+        env_list = [env_default]
+
+    # initialize transistor database from simulation data
+    nch_db = MOSDBDiscrete(spec_list, interp_method=interp_method)
+    # set process corners
+    nch_db.env_list = env_list
+    # set layout parameters
+    nch_db.set_dsn_params(intent=intent)
+    # returns a dictionary of smal-signal parameters
+    return nch_db.query(vbs=vbs, vds=vds, vgs=vgs, vstar=vstar)
+
+
+def plot_data(name='ibias', bounds=None, unit_val=None, unit_label=None):
+    """Get interpolation function and plot/query."""
+    env_list = [env_default]
     vbs = 0.0
     nvds = 41
     nvgs = 81
+    spec_list = [spec_file]
 
-    mos_config = bag.BagProject().tech_info.tech_params['mos']
-    root_dir = mos_config['mos_char_root']
+    print('create transistor database')
+    nch_db = MOSDBDiscrete(spec_list, interp_method=interp_method)
+    nch_db.env_list = env_list
+    nch_db.set_dsn_params(intent=intent)
 
-    db = bag.tech.mos.MosCharDB(root_dir, mos_type, ['intent', 'l'],
-                                env_list, intent=intent, l=l,
-                                method='spline')
-
-    f = db.get_function(name)
-    params, prange = db.get_fun_sweep_params()
-
-    vds_min2, vds_max = prange[params.index('vds')]
-    vgs_min2, vgs_max = prange[params.index('vgs')]
-
-    if vgs_min is None:
-        vgs_min = vgs_min2
-    if vds_min is None:
-        vds_min = vds_min2
-    vgs_min = max(vgs_min, vgs_min2)
-    vds_min = max(vds_min, vds_min2)
+    f = nch_db.get_function(name)
+    vds_min, vds_max = f.get_input_range(1)
+    vgs_min, vgs_max = f.get_input_range(2)
+    if bounds is not None:
+        if 'vgs' in bounds:
+            v0, v1 = bounds['vgs']
+            if v0 is not None:
+                vgs_min = max(vgs_min, v0)
+            if v1 is not None:
+                vgs_max = min(vgs_max, v1)
+        if 'vds' in bounds:
+            v0, v1 = bounds['vds']
+            if v0 is not None:
+                vds_min = max(vds_min, v0)
+            if v1 is not None:
+                vds_max = min(vds_max, v1)
 
     # query values.
-    pprint.pprint(db.query(w=w, vbs=vbs,
-                           vgs=(vgs_min + vgs_max) / 2.0,
-                           vds=(vds_min + vds_max) / 2.0))
+    vds_test = (vds_min + vds_max) / 2
+    vgs_test = (vgs_min + vgs_max) / 2
+    pprint.pprint(nch_db.query(vbs=vbs, vds=vds_test, vgs=vgs_test))
 
+    vbs_vec = [vbs]
     vds_vec = np.linspace(vds_min, vds_max, nvds, endpoint=True)
     vgs_vec = np.linspace(vgs_min, vgs_max, nvgs, endpoint=True)
-
-    w, vbs, vds, vgs = np.meshgrid([w], [vbs], vds_vec, vgs_vec, indexing='ij')
-
-    arg = np.stack([w, vbs, vds, vgs], axis=4)
+    vbs_mat, vds_mat, vgs_mat = np.meshgrid(vbs_vec, vds_vec, vgs_vec, indexing='ij', copy=False)
+    arg = np.stack((vbs_mat, vds_mat, vgs_mat), axis=-1)
     ans = f(arg)
 
-    vds = vds.reshape([nvds, nvgs])
-    vgs = vgs.reshape([nvds, nvgs])
-    ans = ans.reshape([nvds, nvgs, len(env_list)])
+    vds_mat = vds_mat.reshape((nvds, nvgs))
+    vgs_mat = vgs_mat.reshape((nvds, nvgs))
+    ans = ans.reshape((nvds, nvgs, len(env_list)))
 
+    formatter = ticker.ScalarFormatter(useMathText=True)
+    formatter.set_scientific(True)
+    formatter.set_powerlimits((-2, 3))
+    if unit_label is not None:
+        zlabel = '%s (%s)' % (name, unit_label)
+    else:
+        zlabel = name
     for idx, env in enumerate(env_list):
         fig = plt.figure(idx + 1)
         ax = fig.add_subplot(111, projection='3d')
-        print('%s: %s = %.4g' % (env, name, ans[-1, -1, idx]))
-        ax.plot_surface(vds, vgs, ans[..., idx], rstride=1, cstride=1, linewidth=0, cmap=cm.cubehelix)
-        ax.set_title('%s__%s' % (name, env))
-        ax.set_xlabel('Vds')
-        ax.set_ylabel('Vgs')
+        cur_val = ans[..., idx]
+        if unit_val is not None:
+            cur_val = cur_val / unit_val
+        ax.plot_surface(vds_mat, vgs_mat, cur_val, rstride=1, cstride=1, linewidth=0, cmap=cm.cubehelix)
+        ax.set_title('%s (corner=%s)' % (name, env))
+        ax.set_xlabel('Vds (V)')
+        ax.set_ylabel('Vgs (V)')
+        ax.set_zlabel(zlabel)
+        ax.w_zaxis.set_major_formatter(formatter)
 
     plt.show()
-
-
-def optimize(adj_bias=True, opt_package='pyoptsparse', opt_method='IPOPT'):
-    """Find w and vgs that optimizes the gain."""
-    env_list = ['tt', 'ff']
-    l = 60e-9
-    intent = 'lvt'
-    debug = False
-
-    mos_config = bag.BagProject().tech_info.tech_params['mos']
-    root_dir = mos_config['mos_char_root']
-
-    db = bag.tech.mos.MosCharDB(root_dir, 'nch', ['intent', 'l'],
-                                env_list, intent=intent, l=l,
-                                method='spline',
-                                opt_package=opt_package,
-                                opt_method=opt_method,
-                                # opt_settings=dict(
-                                #     
-                                #     ),
-                                )
-
-    ndim = len(env_list)
-    vds_dim = ndim if adj_bias else 1
-    objective = 'obj'
-    define = [
-        # set vds = vgs (vin = vout)
-        ('vds = vgs', vds_dim),
-        # compute gain
-        # ('gain = gm / gds', ndim),
-        # compute bandwidth
-        ('fbw = gds / (2 * 3.14159 * (cdd + 20e-15))', ndim),
-        # compute ro
-        ('ro = 1.0 / gds', ndim),
-        # set to maximum worst case gain
-        ('obj = -min(gain)', 1),
-    ]
-
-    cons = dict(
-        # constrain minimum bandwidth
-        fbw=dict(lower=9e8),
-    )
-
-    # we can adjust vgs/vds over process corners.
-    vector_params = {'vgs', 'vds'} if adj_bias else set()
-
-    # minimize, set vbs = 0
-    results = db.minimize(objective, define=define, cons=cons,
-                          vector_params=vector_params, vbs=0, debug=debug)
-
-    pprint.pprint(results)
